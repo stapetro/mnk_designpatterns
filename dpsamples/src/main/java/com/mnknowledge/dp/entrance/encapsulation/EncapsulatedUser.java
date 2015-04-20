efadb984ddf7191741ca26e0ecd1bf38c821045f
Stanislav Petrov <stanislav.i.petrov@gmail.com>
2015-04-20 23:59:34 +0300

    Improves toString method
    Signed-off-by: Stanislav Petrov <stanislav.i.petrov@gmail.com>

--- dpsamples/src/main/java/com/mnknowledge/dp/entrance/encapsulation/EncapsulatedUser.java
+++ dpsamples/src/main/java/com/mnknowledge/dp/entrance/encapsulation/EncapsulatedUser.java
@@ -36,7 +36,7 @@ public class EncapsulatedUser {
 
     @Override
     public String toString() {
-        return String.format("%s -> %s", getFullName(), getEmail());
+        return "EncapsulatedUser [email=" + getEmail() + ", fullName=" + getFullName() + "]";
     }
 
 }

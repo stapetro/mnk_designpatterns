6c335a8185248588da10f6be394763acabeabe10
Stanislav Petrov <stanislav.i.petrov@gmail.com>
2018-11-24 18:18:01 +0200

    Improve encapsulated user

--- dpsamples/src/main/java/com/mnknowledge/dp/entrance/encapsulation/UserPresenter.java
+++ dpsamples/src/main/java/com/mnknowledge/dp/entrance/encapsulation/UserPresenter.java
@@ -1,18 +1,34 @@
 package com.mnknowledge.dp.entrance.encapsulation;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class UserPresenter {
 
     public static void main(String[] args) {
-        final String[] FIRST_NAME = { "Petko", "Bjarne", "Robert", "Martin" };
-        final String[] LAST_NAME = { "Ivanov", "Stroustrup", "Martin", "Folwer" };
-        final String[] EMAIL = { "petko.ivanov@gmail.com", "stroustrup@gmail.com", "ruby@unclebob.com",
-                "uml@martifolwer.com" };
-        UserPresenter presenter = new UserPresenter();
-        Collection<User> users = presenter.getUsers(FIRST_NAME, LAST_NAME, EMAIL);
-        Collection<Object> encapsulatedUsers = presenter.getEncapsulatedUsers(FIRST_NAME, LAST_NAME, EMAIL);
+        final String[] FIRST_NAME = {"Petko", "Bjarne", "Robert", "Martin"};
+        final String[] LAST_NAME = {"Ivanov", "Stroustrup", "Martin", "Folwer"};
+        final String[] EMAIL = {"petko.ivanov@gmail.com", "stroustrup@gmail.com", "ruby@unclebob.com",
+                "uml@martifolwer.com"};
+        final IntFunction<User> createUserFn = index -> {
+            final User user = new User();
+            user.firstName = FIRST_NAME[index];
+            user.lastName = LAST_NAME[index];
+            user.email = EMAIL[index];
+            return user;
+        };
+        final IntFunction<EncapsulatedUser> createEncUserFn = index -> {
+            final EncapsulatedUser user = new EncapsulatedUser();
+            user.setFirstName(FIRST_NAME[index]);
+            user.setLastName(LAST_NAME[index]);
+            user.setEmail(EMAIL[index]);
+            return user;
+        };
+        final UserPresenter presenter = new UserPresenter();
+        final List<User> users = presenter.getUsers(FIRST_NAME, createUserFn);
+        final List<EncapsulatedUser> encapsulatedUsers = presenter.getUsers(FIRST_NAME, createEncUserFn);
         presenter.printUsers(users);
         presenter.print(encapsulatedUsers);
     }
@@ -20,7 +36,7 @@ public class UserPresenter {
     /**
      * Prints user's full name & email.
      */
-    public void printUsers(Collection<User> users) {
+    public void printUsers(List<User> users) {
         String fullName;
         String email;
         StringBuffer output = new StringBuffer("Users are:\n");
@@ -32,38 +48,15 @@ public class UserPresenter {
         System.out.println(output);
     }
 
-    public void print(Collection<Object> objects) {
-        StringBuffer output = new StringBuffer("Objects are:\n");
+    public <T> void print(List<T> objects) {
+        final StringBuffer output = new StringBuffer("Objects are:\n");
         for (Object object : objects) {
             output.append(object).append("\n");
         }
         System.out.println(output);
     }
 
-    public Collection<User> getUsers(final String[] FIRST_NAME, final String[] LAST_NAME, final String[] EMAIL) {
-        Collection<User> users = new ArrayList<User>();
-        User user;
-        for (int index = 0; index < FIRST_NAME.length; index++) {
-            user = new User();
-            user.firstName = FIRST_NAME[index];
-            user.lastName = LAST_NAME[index];
-            user.email = EMAIL[index];
-            users.add(user);
-        }
-        return users;
-    }
-
-    public Collection<Object> getEncapsulatedUsers(final String[] FIRST_NAME, final String[] LAST_NAME,
-            final String[] EMAIL) {
-        Collection<Object> users = new ArrayList<>();
-        EncapsulatedUser user;
-        for (int index = 0; index < FIRST_NAME.length; index++) {
-            user = new EncapsulatedUser();
-            user.setFirstName(FIRST_NAME[index]);
-            user.setLastName(LAST_NAME[index]);
-            user.setEmail(EMAIL[index]);
-            users.add(user);
-        }
-        return users;
+    public <T> List<T> getUsers(final String[] firstNames, IntFunction<T> createUserFn) {
+        return IntStream.range(0, firstNames.length).mapToObj(createUserFn).collect(Collectors.toList());
     }
 }
--- dpsamples/src/main/java/com/mnknowledge/dp/structural/flyweight/ShapeFactory.java
+++ dpsamples/src/main/java/com/mnknowledge/dp/structural/flyweight/ShapeFactory.java
@@ -1,17 +1,21 @@
 package com.mnknowledge.dp.structural.flyweight;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ShapeFactory {
+    private static final Logger logger = LoggerFactory.getLogger(ShapeFactory.class);
 
     private final Map<Color, Shape> shapeByColorPool = new HashMap<>();
 
     public Shape getShapeBy(Color color) {
         Shape shape = shapeByColorPool.get(color);
         if (shape == null) {
-            System.out.println("Create shape with color " + color.toString());
+            logger.info("Create shape with color {}", color.toString());
             shape = createNewShape();
             shape.setColor(color);
             shapeByColorPool.put(color, shape);
